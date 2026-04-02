8ac239153671bfd83a3e0ebd9552c119a9dad134
0837a192 <adamovbers@gmail.com>
2026-04-02 12:34:57 +0300

    Nothing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include "pch.hpp"
 
-
-//without enter input
-
 int getch() {
     struct termios oldt, newt;
     int ch;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     ch = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
--- pch.hpp
+++ pch.hpp
@@ -1,9 +1,12 @@
+#pragma once
 #include <chrono>
 #include <iostream>
 #include <limits>
 #include <random>
 #include <string>
+#include <string_view>
 #include <termios.h>
 #include <thread>
 #include <unistd.h>
 #include <vector>
+
